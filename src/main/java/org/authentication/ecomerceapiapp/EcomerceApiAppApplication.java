1ebcb001b01adaaaee7df59f1ca1d324faf4114b
NahuelGuillermoViera <nahuelguillermoviera@gmail.com>
2025-01-03 13:04:45 -0300

    FirstCommit

---
+++ src/main/java/org/authentication/ecomerceapiapp/EcomerceApiAppApplication.java
@@ -0,0 +1,13 @@
+package org.authentication.ecomerceapiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomerceApiAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomerceApiAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/authentication/ecomerceapiapp/EcomerceApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.authentication.ecomerceapiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerceApiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
